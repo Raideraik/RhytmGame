dd39a1bd294073e5e2c249a9eed5459444b67ed3
Raideraik <denkamartinenko@gmail.com>
2023-02-06 00:31:51 +0200

    Camera

--- Assets/Scripts/CameraDebug.cs
+++ Assets/Scripts/CameraDebug.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class CameraDebug : MonoBehaviour
 {
     [SerializeField] private Camera _arCamera;
     [SerializeField] private TMP_Text _text;
-
     private void Update()
     {
         _text.text = _arCamera.fieldOfView.ToString();
-        _arCamera.fieldOfView = 60f;
+     //  _arCamera.fieldOfView = 60f;
     }
 
 }
